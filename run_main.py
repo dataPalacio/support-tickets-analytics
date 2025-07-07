3ddb8bac938df605dee42e4d6efaa36377e972de
Gustavo Palacio <palacio.dados@gmail.com>
2025-07-07 11:26:53 -0300

    Atualização do diretório run_main.py

--- run_main.py
+++ run_main.py
@@ -1,6 +1,6 @@
 from scripts.main import exec_pipeline
 
-caminho_arquivo = r'C:\git-clones\support-tickets-analytics\data\raw\2025-07-01_raw.csv'  # Ajuste o caminho se necessário
-nome_arquivo_saida = r'C:\git-clones\support-tickets-analytics\data\processed\all_gau_processed.csv'  # Ajuste o caminho se necessário
+caminho_arquivo = r'data\raw\2025-07-01_raw.csv'  # Ajuste o caminho se necessário
+nome_arquivo_saida = r'data\processed\2025-07-01_processed.csv'  # Ajuste o caminho se necessário
 
 exec_pipeline(caminho_arquivo, nome_arquivo_saida)
